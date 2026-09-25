c912d802665346455513445d4fbda251e8167a5f
Huy Doan <tui@pm.me>
2026-09-25 23:08:38 +0700

    chore: use ba0f3 module path

--- internal/api/projects.go
+++ internal/api/projects.go
@@ -3,7 +3,7 @@ package api
 import (
 	"fmt"
 
-	"github.com/rohithmahesh3/plane-cli/pkg/plane"
+	"github.com/ba0f3/plane-cli/pkg/plane"
 )
 
 func (c *Client) ListProjects() ([]plane.Project, error) {
