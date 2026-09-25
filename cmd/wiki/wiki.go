a3998fe7d446dd5140d5cbf7e4b9d1f80895c5fc
Huy Doan <tui@pm.me>
2026-09-25 23:07:09 +0700

    chore: use ba0f3 module path

--- cmd/wiki/wiki.go
+++ cmd/wiki/wiki.go
@@ -3,9 +3,9 @@ package wiki
 import (
 	"fmt"
 
-	"github.com/rohithmahesh3/plane-cli/internal/api"
-	"github.com/rohithmahesh3/plane-cli/internal/config"
-	"github.com/rohithmahesh3/plane-cli/internal/output"
+	"github.com/ba0f3/plane-cli/internal/api"
+	"github.com/ba0f3/plane-cli/internal/config"
+	"github.com/ba0f3/plane-cli/internal/output"
 	"github.com/spf13/cobra"
 )
 
