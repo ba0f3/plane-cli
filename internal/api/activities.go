03b841ed3a4212efdc74a6b999d527d870cde002
Huy Doan <tui@pm.me>
2026-09-25 23:07:25 +0700

    chore: use ba0f3 module path

--- internal/api/activities.go
+++ internal/api/activities.go
@@ -3,7 +3,7 @@ package api
 import (
 	"fmt"
 
-	"github.com/rohithmahesh3/plane-cli/pkg/plane"
+	"github.com/ba0f3/plane-cli/pkg/plane"
 )
 
 // ListActivities retrieves all activities for an issue
