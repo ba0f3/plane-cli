34fda9a61409b90919725d8b7f93d70d6ec6a89b
Huy Doan <tui@pm.me>
2026-09-25 23:07:20 +0700

    chore: use ba0f3 module path

--- cmd/workspace/workspace.go
+++ cmd/workspace/workspace.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
-	"github.com/rohithmahesh3/plane-cli/internal/api"
-	"github.com/rohithmahesh3/plane-cli/internal/config"
-	"github.com/rohithmahesh3/plane-cli/internal/output"
+	"github.com/ba0f3/plane-cli/internal/api"
+	"github.com/ba0f3/plane-cli/internal/config"
+	"github.com/ba0f3/plane-cli/internal/output"
 	"github.com/spf13/cobra"
 )
 
